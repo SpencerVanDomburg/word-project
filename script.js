364fbdd1b3e791d36f04ecefca57e0efab894f8f
Spencer <spencer676@hotmail.com>
2021-01-08 15:22:10 +0100

    updates on project

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 const questionField = document.getElementById("question-field");
+let answerField = document.getElementById("answer-field");
 const checkAnswerButton = document.getElementById("check-answer-btn");
 
-const wordArray = ["Aap", "Noot", "Mies", "Kees"];
+const wordArray = [
+  { Question: "Aap", Answer: "Monkey" },
+  { Question: "Noot", Answer: "Nut" },
+  { Question: "Molen", Answer: "Mill" },
+  { Question: "Boom", Answer: "Tree" },
+];
 
 function getRandomWord() {
   let randomWordFromArray =
-    wordArray[Math.floor(Math.random() * wordArray.length)];
+    wordArray[Math.floor(Math.random() * wordArray.length)].Question;
   document.getElementById("question-field").value = randomWordFromArray;
+}
+
+function saveAnswer() {
+  console.log("called saveAnswer()");
+  answerField = document.getElementById("answer-field").value;
 }
 
 getRandomWord();
 
-// checkAnswerButton.addEventListener("click")
+checkAnswerButton.addEventListener("click", () => {
+  if (document.getElementById("answer-field") === null) {
+    alert("enter something");
+  } else {
+    answerField = document.getElementById("answer-field").value;
+    if (questionField === answerField) {
+      alert("correct!");
+      console.log("correct");
+    } else {
+      alert("not correct");
+      console.log("incorrect");
+    }
+  }
+});
